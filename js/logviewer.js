0432104d6537b1840b09872877ddc2a33b8d50f6
Yoshiki Shibukawa <shibukawa.yoshiki@dena.jp>
2014-10-30 14:20:43 +0900

    final version

--- js/logviewer.js
+++ js/logviewer.js
@@ -8,13 +8,53 @@ $('.input-group.date').datepicker({
 
 $('.input-group.date').datepicker('update', new Date());
 
-hljs.initHighlightingOnLoad();
+var allLogs = {};
+
+var myViewModel = {
+  sessions: ko.observableArray(),
+  logs: ko.observableArray()
+};
+
+function SessionModel(session) {
+    this.session = session;
+    this.active = ko.observable(false);
+    this.click = function () {
+        sessions = myViewModel.sessions();
+        console.log("click", sessions.length);
+        for (var i = 0; i < sessions.length; i++) {
+            sessions[i].active(this == sessions[i]);
+        }
+        this.active(true);
+        myViewModel.logs.removeAll();
+        var sessions = allLogs[this.session];
+        for (var i = 0; i < sessions.length; i++) {
+            myViewModel.logs.push(sessions[i]);
+        }
+        hljs.initHighlightingOnLoad();
+    };
+}
+
+ko.applyBindings(myViewModel);
 
 $('#query').submit(function (event) {
-    console.log($("#user-input").val());
-    console.log($("#date-input").val());
     $.get('/search', {user: $("#user-input").val(), date: $("#date-input").val()}, function (data) {
-        console.log("result: ", data);
+        var lines = data.split("\n");
+        myViewModel.sessions.removeAll();
+        allLogs = {};
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            var cleanedLine = line.split($("#date-input").val() + "T")[1];
+            if (!cleanedLine) {
+                continue;
+            }
+            var tokens = cleanedLine.split("\t");
+            var session = tokens[1].slice(tokens[1].lastIndexOf(".") + 1);
+            if (!allLogs.hasOwnProperty(session)) {
+                allLogs[session] = [];
+                myViewModel.sessions.push(new SessionModel(session));
+            }
+            allLogs[session].push({date: tokens[0], log: tokens[2]});
+        }
     })
     return false;
 });
--- logviewer.go
+++ logviewer.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
+	"path/filepath"
 )
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Query()["user"][0])
-	arg := fmt.Sprintf("^%s.*\\t%s", r.URL.Query()["date"][0], r.URL.Query()["user"][0])
-	fmt.Println(arg)
-	out, err := exec.Command("grep", arg, "/Users/shibukawa.yoshiki/develop/logserver/log/*").CombinedOutput()
+	log.Printf("query: date=%s user=%s\n", r.URL.Query()["date"][0], r.URL.Query()["user"][0])
+	pattern := fmt.Sprintf("^%s.*\\t%s", r.URL.Query()["date"][0], r.URL.Query()["user"][0])
+	files, err := filepath.Glob("./logfiles/*.log")
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println(string(out))
+		log.Println(err)
 		fmt.Fprintf(w, "")
 		return
 	}
-	fmt.Println(out)
+	if len(files) == 0 {
+		log.Println("no file matches")
+		fmt.Fprintf(w, "")
+		return
+	}
+	args := make([]string, len(files)+1)
+	args[0] = pattern
+	for i := 0; i < len(files); i++ {
+		args[i+1] = files[i]
+	}
+	out, err := exec.Command("grep", args...).CombinedOutput()
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "")
+		return
+	}
 	fmt.Fprintf(w, string(out))
 }
 
